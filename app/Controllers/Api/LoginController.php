5ee264615e8a5428a7577dc3526913379d700594
heropoo <aiyouyou1000@163.com>
2021-09-25 18:22:01 +0800

    log

--- app/Controllers/Api/LoginController.php
+++ app/Controllers/Api/LoginController.php
@@ -8,11 +8,15 @@ namespace App\Controllers\Api;
 
 
 use App\Models\SmsCaptcha;
+use Monolog\Logger;
 
 class LoginController
 {
     public function loginCaptcha()
     {
+        /** @var Logger $logger */
+        $logger = \App::$container->get('logger');
+        $logger->info(__METHOD__.':'.json_encode($_POST));
         $phone = $_POST['phone'] ?? '';
         if (empty($phone)) {
             return returnJson(400, "请输入正确的手机号码");
