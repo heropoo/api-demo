7e93afc590a7f1581daa47bde731bd7dbe200b45
heropoo <aiyouyou1000@163.com>
2021-10-03 14:35:38 +0800

    bin

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -13,7 +13,7 @@ async fn index2(web::Path((id, name)): web::Path<(u32, String)>) -> impl Respond
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8000")?
+        .bind("0.0.0.0:8002")?
         .run()
         .await
 }
