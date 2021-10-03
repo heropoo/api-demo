51fb3dca52f5655a30882bf021710d8f66388072
heropoo <aiyouyou1000@163.com>
2021-10-03 10:56:23 +0800

    rust

---
+++ rust/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("welcome")
+}
+
+#[get("/{id}/{name}/index.html")]
+async fn index2(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
