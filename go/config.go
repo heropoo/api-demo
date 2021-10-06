16a2d3b694c68572ef7d1e2fa3d3fd35c1597e56
heropoo <aiyouyou1000@163.com>
2021-10-06 18:44:52 +0800

    test

--- go/config.go
+++ go/config.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
@@ -20,19 +22,37 @@ type DBConfigStruct struct {
 	DBName   string `yaml:"dbname"`
 }
 
-func getConfig(filename string) (ConfigStruct, error) {
+func getConfig(filename string) (*ConfigStruct, error) {
 	var config ConfigStruct
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Read yaml file failed, err:", err)
-		return config, err
+		return &config, err
 	}
 	//fmt.Println(string(content))
 
 	err = yaml.Unmarshal([]byte(content), &config)
 	if err != nil {
 		fmt.Println("Create yaml struct failed, err:", err)
-		return config, err
+		return &config, err
+	}
+
+	return &config, nil
+}
+
+func LoadConfig(filename string) (*ConfigStruct, error) {
+	configFile, err := os.Open(filename)
+
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read configuration file %s", filename)
+	}
+
+	config := new(ConfigStruct)
+
+	decoder := json.NewDecoder(configFile)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse configuration file %s", filename)
 	}
 
 	return config, nil
