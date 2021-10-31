2433d5461a0a89cf3690823b3bd8e4c38f509d3e
heropoo <aiyouyou1000@163.com>
2021-10-31 12:11:14 +0800

    test

--- php/app/Controllers/Api/UserController.php
+++ php/app/Controllers/Api/UserController.php
@@ -12,27 +12,28 @@ use Moon\Pagination\Pagination;
 
 class UserController
 {
-    public function index(){
+    public function index()
+    {
         $pageNo = $_GET['pageNo'] ?? 1;
         $pageSize = $_GET['pageSize'] ?? 10;
         $name = $_GET['name'] ?? '';
 
-        $conditon = '1=1';
+        $condition = '1=1';
         $params = [];
-        if(strlen(trim($name)) > 0){
-            $conditon .= ' AND `username`=?';
+        if (strlen(trim($name)) > 0) {
+            $condition .= ' AND `username`=?';
             $params[] = $name;
         }
 
         //$query = User::find()->where("1=1");
-        $total = User::find()->where($conditon, $params)->count();
+        $total = User::find()->where($condition, $params)->count();
         $pagination = new Pagination($total, $pageSize, 2, 'pageNo');
-        $list = User::find()->where($conditon, $params)
+        $list = User::find()->where($condition, $params)
             ->limit($pagination->getLimit())->offset($pagination->getOffset())
             ->order('id desc')
             ->all();
         return returnJson(0, "success", [
-            'total'=>$total,
+            'total' => $total,
             'list' => $list
         ]);
     }
